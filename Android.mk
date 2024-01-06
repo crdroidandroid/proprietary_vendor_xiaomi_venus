d48e839cb45279e6bc6760c35b6ef033b8667293
FlowerSea0208 <zzp060208@gmail.com>
2024-01-06 23:50:38 +0800

    venus: Import firmware from venus V14.0.7.0.TKBMIXM

--- Android.mk
+++ Android.mk
@@ -6,4 +6,22 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),venus)
 
+$(call add-radio-file-sha1-checked,radio/abl.img,54bf99992b17037ae44682ca5beb6369833ccb44)
+$(call add-radio-file-sha1-checked,radio/aop.img,a90cf2be38a1112b1853de730e2a56054ba62155)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,9d32624e7e331383a53ae0df810b2ee2fc46bcc0)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,4ea554473651cca61d0f3db87c680df02625031f)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,90ab435a2da4af030cbef5befc1cf928934d9193)
+$(call add-radio-file-sha1-checked,radio/dsp.img,8b27181b72bcc6cc003e5c21bac38993e7015995)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,4fe61645d5c99d35bff880162ced69c76c4dfb26)
+$(call add-radio-file-sha1-checked,radio/hyp.img,021a2dc2c42662679b23c741776c41c209f1f053)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,54fd4ec9ef6eaba6d8be6a804bc1156cd6602b3a)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,5f9e558f334334c47d5135b2eac76d73778e65b2)
+$(call add-radio-file-sha1-checked,radio/modem.img,4a122ac0616219ceb454a6bb065bcee9200bc717)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,a15f1d7981d334b904839797038ef6032cf5cdf2)
+$(call add-radio-file-sha1-checked,radio/shrm.img,6b014bfa72deacb7ea7e44662a95d9a78aebad2d)
+$(call add-radio-file-sha1-checked,radio/tz.img,e049abad2dd336a6153b091726ae83bd0165726d)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,82e6153bc7077955b478794070f17468171e3cca)
+$(call add-radio-file-sha1-checked,radio/xbl.img,fb11ec71b1ad3f9e2957d2883503e6ed8b5a78fe)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,660aafa3823e6141714d6183d701fd4e36c12014)
+
 endif
